99b6d895004460602e1e7a07ab69a9105c6b82cc
riya5624 <riyanegi658@gmail.com>
2026-01-07 21:07:38 +0530

    message

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/background.jpg')),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Sign in to contunue'),
+                  Text(
+                    'Vegi',
+                    style:
+                        TextStyle(fontSize: 50, color: Colors.white, shadows: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Colors.green.shade900,
+                        offset: Offset(3, 3),
+                      )
+                    ]),
+                  ),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Apple,
+                        text: "Sign in with Apple",
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign in with Google",
+                        onPressed: () {
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'By signing in you are agreeing to our',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                      Text(
+                        'Terms and Pricacy Policy',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
